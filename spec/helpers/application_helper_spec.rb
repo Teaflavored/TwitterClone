624c8e51ccdeef358defd6d4faab9b4393d8e965
Teaflavored <auster.s.chen@gmail.com>
2014-08-22 23:31:56 -0700

    Added Tests

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper.rb'
+
+
+describe ApplicationHelper do
+	describe "full_title" do 
+
+		it "should include the page title" do 
+			expect(full_title("foo")).to match(/foo/)
+		end
+
+		it "should include the base title" do
+			expect(full_title("foo")).to match(/^My App/)
+		end
+
+		it "should not include bar" do
+			expect(full_title("")).not_to match(/\|/)
+		end
+	end
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,28 +4,56 @@ describe "StaticPages" do
 
   subject{page}
 
+  shared_examples_for "All Static Pages" do
+    it {should have_selector("h1", heading)}
+    it {should have_title(full_title(pagetitle))}
+  end
+
   describe "Home Page" do
     before {visit root_path}
-    it {should have_content("My App")}
-    it {should have_title(full_title(""))}
+    let(:heading) {"My App"}
+    let(:pagetitle) {""}
+    it_should_behave_like "All Static Pages"
     it {should_not have_title("| Home")}
   end
 
   describe "Help Page" do
   	before {visit help_path}
-    it {should have_content("Help")}
-    it {should have_title(full_title("Help"))}
+    let(:heading) {"Help"}
+    let(:pagetitle) {"Help"}
+    it_should_behave_like "All Static Pages"
   end
 
   describe "About Page" do
   	before {visit about_path}
-    it {should have_content("About")}
-    it {should have_title(full_title("About"))}
+    let(:heading) {"About"}
+    let(:pagetitle) {"About"}
+    it_should_behave_like "All Static Pages"
   end
 
   describe "Contact Page" do
     before {visit contact_path}
-    it {should have_content("Contact")}
-    it {should have_title(full_title("Contact"))}
+    let(:heading) {"Contact"}
+    let(:pagetitle) {"Contact"}
+    it_should_behave_like "All Static Pages"
   end
+
+  describe "Links" do
+    it "should link to right places" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_title(full_title("About"))
+      click_link "Contact"
+      expect(page).to have_title(full_title("Contact"))
+      click_link "Help"
+      expect(page).to have_title(full_title("Help"))
+      click_link "Home"
+      expect(page).to have_title(full_title(""))
+      click_link "Sign Up"
+      expect(page).to have_title(full_title("Sign Up"))
+      click_link "My App"
+      expect(page).to have_title(full_title(""))
+    end
+  end
+
 end
